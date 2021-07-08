e93675f50c6bd81e91e30450230fa350874d95c1
hkc <hatkidchan@gmail.com>
2021-07-09 00:59:56 +0300

    Fixed library
    And also:
     * Moved some variables to plugin namespace
     * Moved getting food data to spawn handler because we don't know
       anything before logging in to server
     * Removed all logging because why not
     * Arrow functions go brr

--- index.js
+++ index.js
@@ -1,106 +1,91 @@
 module.exports = function (bot, options) {
-	var disabled = false
-
-	bot.autoEat = {}
+	bot.autoEat = { disabled: false, isEating: false }
 
 	bot.autoEat.disable = function disable() {
-		disabled = true
+		bot.autoEat.disabled = true
 	}
 
 	bot.autoEat.enable = function enable() {
-		disabled = false
+		bot.autoEat.disabled = false
 	}
 
-	bot.autoEat.eat = eat
+	bot.autoEat.eat = eat;
 
-	bot.autoEat.options = {}
-	bot.autoEat.options.priority = options.priority || 'foodPoints'
-	bot.autoEat.options.startAt = options.startAt || 14
-	bot.autoEat.options.bannedFood = options.bannedFood || []
+	bot.autoEat.options = {};
+	bot.autoEat.options.priority = options.priority || 'foodPoints';
+	bot.autoEat.options.startAt = options.startAt || 14;
+	bot.autoEat.options.bannedFood = options.bannedFood || [];
 
-	var isEating = false
+	bot.autoEat._foodsByName = {};
 
-	const mcData = require('minecraft-data')(bot.version)
-
-	const foodData = mcData.foodsArray
-	const foodNames = foodData.map((item) => item.name)
-
-	function callbackHandle(err) {
-		if (err) console.error(err)
-	}
+	bot.once('spawn', () => {
+		bot.autoEat.foodsByName = require('minecraft-data')(bot.version).foodsByName;
+	});
 
 	function eat(callback, manual = false) {
-		if (isEating) return callback(Error("Already eating"))
+		callback = callback || ((e) => { }); // fallback callback that does nothing
+		if (bot.autoEat.isEating) return callback(Error("Already eating"));
 
-		isEating = true
+		bot.autoEat.isEating = true;
 
-		var best_food = null
-		var best_points = -1
-
-		var priorityProperty = bot.autoEat.options.priority === 'foodPoints' ? 'foodPoints' : 'saturation'
+		var best_food = null;
+		var best_points = -1;
+		const priorityProperty = bot.autoEat.options.priority;
 
 		for (const item of bot.inventory.items()) {
-			if (best_points < item[priorityProperty]) {
-				if (foodNames.includes(item.name) && !bot.autoEat.options.bannedFood.includes(item.name)) {
-					best_food = item
-					best_points = item[priorityProperty];
-				}
+			if (!(item.name in bot.autoEat.foodsByName))
+				continue; // Skip non-food items
+			if (bot.autoEat.options.bannedFood.includes(item.name))
+				continue; // Skip banned food
+			const props = bot.autoEat.foodsByName[item.name];
+			if (best_points < props[priorityProperty]) {
+				best_points = props[priorityProperty];
+				best_food = item;
 			}
 		}
 
 		if (!best_food) {
-			isEating = false
-			if (!manual) return callback(null)
-			else return callback(new Error('No Food found.'))
+			bot.autoEat.isEating = false;
+			if (!manual) return callback(null);
+			else return callback(new Error('No Food found.'));
 		}
 
-		bot.emit('autoeat_started')
+		bot.emit('autoeat_started', best_food);
 
-		bot.equip(best_food, 'hand', function (error) {
+		bot.equip(best_food, 'hand', (error) => {
 			if (error) {
-				console.error(error)
-				bot.emit('autoeat_stopped')
-				isEating = false
-				return callback(error)
-			} else {
-				bot.consume(function (err) {
-					if (err) {
-						console.error(err)
-						bot.emit('autoeat_stopped')
-						isEating = false
-						return callback(err)
-					} else {
-						isEating = false
-						bot.emit('autoeat_stopped')
-						callback(null)
-						if (bot.food !== 20) eat(callbackHandle)
-					}
-				})
+				bot.emit('autoeat_stopped', error);
+				bot.autoEat.isEating = false;
+				return callback(error);
 			}
-		})
+			bot.consume((err) => {
+				bot.emit('autoeat_stopped', err);
+				bot.autoEat.isEating = false;
+				if (err) {
+					return callback(err);
+				} else {
+					callback(null);
+					if (bot.food !== 20) eat();
+				}
+			});
+		});
 	}
 
 	bot.on('health', () => {
+		if (bot.autoEat.disabled) return;
+		if (bot.food >= bot.autoEat.options.startAt) return;
 		if (bot.pathfinder) {
-			if (
-				bot.food < bot.autoEat.options.startAt &&
-				!(bot.pathfinder.isMining() || bot.pathfinder.isBuilding()) &&
-				disabled === false
-			) {
-				eat(callbackHandle)
-			}
-		} else {
-			if (bot.food < bot.autoEat.options.startAt && disabled === false) {
-				eat(callbackHandle)
-			}
+			if (bot.pathfinder.isMining() || bot.pathfinder.isBuilding())
+				return;
 		}
+		bot.autoEat.eat();
 	})
 
 	bot.on('spawn', () => {
-		isEating = false // to prevent the plugin from breaking if the bot gets killed while eating btw
+		bot.autoEat.isEating = false; // Eating status is reset on spawn/death
 	})
 
 	bot.on('death', () => {
-		isEating = false // to prevent the plugin from breaking if the bot gets killed while eating btw
+		bot.autoEat.isEating = false;
 	})
 }
